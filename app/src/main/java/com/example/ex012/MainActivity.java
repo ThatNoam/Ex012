b7a14247050a6b69ef616401ff2853c80895ac4b
ThatNoam <noamk574@gmail.com>
2026-03-23 01:18:32 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ex012/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.ex012;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    ToggleButton tb;
+    Switch sw;
+    Button btn;
+    LinearLayout layout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
